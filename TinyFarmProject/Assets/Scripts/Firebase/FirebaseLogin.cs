87742b7a37ab2eafc4fcbcf158b4275e842ede13
Nguyen Nhat Quan <NguyenNhatQuan06022004@gmail.com>
2025-11-25 17:52:35 +0700

    login with firebase 2

--- TinyFarmProject/Assets/Scripts/Firebase/FirebaseLogin.cs
+++ TinyFarmProject/Assets/Scripts/Firebase/FirebaseLogin.cs
@@ -1,13 +1,27 @@
-using Firebase.Auth;
+﻿using Firebase.Auth;
 using Firebase.Extensions;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class FirebaseLogin : MonoBehaviour
 {
+    [Header("Register")]
     public InputField ipRegisterEmail;
     public InputField ipRegisterPassword;
     public Button buttonRegister;
+
+    [Header("Sign In")]
+    public InputField ipLoginEmail;
+    public InputField ipLoginPassword;
+    public Button buttonLogin;
+
+    [Header("Switch form")]
+    public Button buttonMoveToSignIn;
+    public Button buttonMoveToRegister;
+
+    public GameObject loginForm;
+    public GameObject registerForm;
 
     private FirebaseAuth auth;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,33 +29,63 @@ public class FirebaseLogin : MonoBehaviour
     {
         auth = FirebaseAuth.DefaultInstance;
         buttonRegister.onClick.AddListener(RegisterAccountFirebase);
-
+        buttonLogin.onClick.AddListener(SignInAccountWithFireBase);
+        buttonMoveToRegister.onClick.AddListener(SwitchForm);
+        buttonMoveToSignIn.onClick.AddListener(SwitchForm);
     }
 
     public void RegisterAccountFirebase()
     {
         string email = ipRegisterEmail.text;
         string password = ipRegisterPassword.text;
-
         auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task => {
-            if(task.IsCanceled)
+            if (task.IsCanceled)
             {
                 Debug.Log("Dang ki bi huy");
                 return;
             }
-            if(task.IsFaulted)
+
+            if (task.IsFaulted)
             {
-                Debug.Log("Dang ki that bai");
+                Debug.Log("Dang ki that bai: " + task.Exception);
+                return;
             }
-            if (task.IsCompleted) {
-                Debug.Log("Dang ki thanh cong");
-            }
+
+            // Chỉ chạy khi thật sự thành công
+            Debug.Log("Dang ki thanh cong");
         });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SignInAccountWithFireBase()
     {
-        
+        string email = ipLoginEmail.text;
+        string password = ipLoginPassword.text;
+
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("Dang nhap bi huy");
+                return;
+            }
+
+            if (task.IsFaulted)
+            {
+                Debug.Log("Dang nhap that bai: " + task.Exception);
+                return;
+            }
+
+            // Chỉ chạy khi thật sự thành công
+            Debug.Log("Dang nhap thanh cong");
+            FirebaseUser user = task.Result.User;
+
+            SceneManager.LoadScene("mapSummer");
+        });
+    }
+
+    public void SwitchForm()
+    {
+        loginForm.SetActive(!loginForm.activeSelf);
+        registerForm.SetActive(!registerForm.activeSelf);
     }
 }
